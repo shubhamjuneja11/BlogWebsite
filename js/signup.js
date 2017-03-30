44755afbcf91de61ec2d6c9a95aca7d0aefff0f9
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-03-30 19:04:00 +0530

    google signin

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+document.getElementById("googlesignin").addEventListener("click",googlesign,false);
     $('#myform').validate({ 
         rules: {
             username:{
@@ -57,3 +57,36 @@ $(document).ready(function () {
     });
 
 });
+    
+/*$("#googlesignin").click(function(){
+console.log("clicked");
+alert("clicked");
+});*/
+
+function googlesign(){
+    console.log("google");
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+
+    provider.setCustomParameters({
+  'login_hint': 'user@example.com'
+});
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user);
+
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
